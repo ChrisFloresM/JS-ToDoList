0cce0efb516f533179efe1f178aeb31b6a8f85a6
Christian <christian.mig.flmo@gmail.com>
2024-12-11 15:28:43 -0600

    Adding functionality to save to localStorage and small tweaks to mobile layout

--- src/js/themeManager.js
+++ src/js/themeManager.js
@@ -1,26 +1,44 @@
+/* Getting the buttons elements */
 const changeThemeButton = document.querySelector(".header__button");
+const themedElements = document.querySelectorAll('.change-theme');
 const buttonIcon = document.querySelector(".header__button-icon");
 
-changeThemeButton.addEventListener("click", (e) => {
-    const darkElements = document.querySelectorAll('.dark');
-    const lightElements = document.querySelectorAll('.light');
+/* Theme to be used as default for any user who opens the app for the first time */
+let defaultThemeObj = retrieveFromStorage("theme", {userTheme: "light"});
+let defaultTheme = defaultThemeObj.userTheme;
+setTheme();
 
-    if (darkElements.length > 0) {
-        darkElements.forEach((el) => {
-            el.classList.remove('dark');
-            el.classList.add('light');
-            buttonIcon.src = 'assets/icon-moon.svg';
-        })
+/* Event listener to the button */
+changeThemeButton.addEventListener("click", () => {
+    defaultTheme = defaultTheme === 'light' ? 'dark' : 'light';
+    setTheme();
+});
+
+function setTheme() {
+    const removeTheme = defaultTheme === 'light' ? 'dark' : 'light';
+
+    themedElements.forEach(element => {
+        element.classList.add(defaultTheme);
+        element.classList.remove(removeTheme);
+    });
+
+    defaultThemeObj.userTheme = defaultTheme;
+    saveToLocalStorage("theme", JSON.stringify(defaultThemeObj));
+}
+
+/* Get the localStorage information about the theme */
+function retrieveFromStorage(key, defaultValue) {
+    let returnObj;
+    try {
+        const storedData = localStorage.getItem(key);
+        returnObj = storedData ? JSON.parse(storedData) : defaultValue;
+    } catch (error) {
+        console.error("Error during parse of local storage items:", error);
+        returnObj = defaultValue;
     }
+    return returnObj;
+}
 
-    if (lightElements.length > 0) {
-        lightElements.forEach((el) => {
-            el.classList.remove('light');
-            el.classList.add('dark');
-            buttonIcon.src = 'assets/icon-sun.svg';
-        })
-    }
-
-
-
-})
+function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
